b51f327461854aeebf053fba0183327c4275b534
Maxx Lehmann <mglehman@mtu.edu>
2017-10-24 00:15:00 +0200

    first button version

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import '../css/Button.css'
+
+class Button extends Component {
+  primary() {
+    if (this.props.primary) {
+      return 'button-primary'
+    }
+  }
+
+  width() {
+    if (this.props.fill) {
+      return '100%'
+    } 
+  }
+
+  render() {
+    return (
+      <button
+        className={this.primary()}
+        style={{
+          width: this.width()
+        }}
+      >
+        {this.props.children}
+      </button>
+    )
+  }
+}
+
+export default Button
--- src/views/Login.js
+++ src/views/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import '../css/Login.css'
 import logo from '../static/logotype_white.svg'
 import Fullpage from '../components/Fullpage'
+import Button from '../components/Button'
 
 class Login extends Component {
   render() {
@@ -9,6 +10,8 @@ class Login extends Component {
       <Fullpage background='#61c5c1' color='white' textAlign='center'>
         <img src={logo} alt='logo' className='logo'/>
         <h3>Make real connections, together</h3>
+        <Button fill>Sign Up</Button>
+        <Button fill>Login</Button>
       </Fullpage>
     )
   }
