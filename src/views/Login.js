e09485f88fe03a7695a9314e66ac4da391c9022d
Maxx Lehmann <mglehman@mtu.edu>
2017-10-23 23:31:48 +0200

    login start

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import '../css/Login.css'
+import logo from '../static/logotype_white.svg'
+import Fullpage from '../components/Fullpage'
 
 class Login extends Component {
   render() {
     return (
-      <h1>Login</h1>
+      <Fullpage background='#61c5c1' color='white' textAlign='center'>
+        <img src={logo} alt='logo' className='logo'/>
+        <h3>Make real connections, together</h3>
+      </Fullpage>
     )
   }
 }
