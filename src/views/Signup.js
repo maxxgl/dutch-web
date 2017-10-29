aa907017e7be4bc58b4d498c33d67dbd06d48c2b
Maxx Lehmann <mglehman@mtu.edu>
2017-10-29 23:04:18 +0100

    added css and header

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import '../css/Signup.css'
 import Fullpage from '../components/Fullpage'
-import Button from '../components/Button'
-import { Column } from '../components/Grid'
+import logo from '../static/logotype_green.svg'
+import { Grid, Column } from '../components/Grid'
 
 class Login extends Component {
   render() {
     return (
-      <Fullpage background='#61c5c1' color='white' textAlign='center'>
-         <Button>Email</Button>
+      <Fullpage>
+        <SignupHeader />
       </Fullpage>
     )
   }
 }
+
+const SignupHeader = () => (
+  <Grid>
+    <Column size='1-4'>
+      <img src={logo} alt='logo' className='signup-logo'/>
+    </Column>
+    <Column size='1-2' />
+    <Column size='1-4' />
+  </Grid>
+)
 
 export default Login
