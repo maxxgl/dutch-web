cd2b6897d8991d1c9be3df0f3bb7f1ee9c196222
Maxx Lehmann <mglehman@mtu.edu>
2017-11-05 23:30:55 +0100

    put in header

--- src/views/Dates.js
+++ src/views/Dates.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import Fullpage from '../components/Fullpage'
+import HomeHeader from '../components/HomeHeader'
 
 export default class Dates extends Component {
   render() {
     return (
-      <div>Dates</div>
+      <Fullpage>
+        <HomeHeader />
+      </Fullpage>
     )
   }
 }
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import Fullpage from '../components/Fullpage'
+import HomeHeader from '../components/HomeHeader'
 
 export default class Profile extends Component {
   render() {
     return (
-      <div>Profile</div>
+      <Fullpage>
+        <HomeHeader />
+      </Fullpage>
     )
   }
 }
