6c8fcd7074608780511411aa5186b5fee7c32e86
Maxx Lehmann <mglehman@mtu.edu>
2017-11-10 18:43:42 +0100

    basic grid thing

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react'
 import '../css/Home.css'
 import Fullpage from '../components/Fullpage'
 import HomeHeader from '../components/HomeHeader'
-// import { Grid, Column } from '../components/Grid'
+import { Grid, Column } from '../components/Grid'
+import girl from '../static/girl.jpg'
+import Header from '../components/Header'
 
 class Home extends Component {
   render() {
     return (
-      <Fullpage>
+      <Fullpage textAlign='center'>
         <HomeHeader />
+        <Grid>
+          <Column size='u-1-1 u-md-1-4' id='home-flavor-text'>
+            <Header className=''>Matches for Friday</Header>
+          </Column>
+          <Column size='u-1-2 u-md-1-4'>
+            <img src={girl} alt='girl' />
+          </Column>
+          <Column size='u-1-2 u-md-1-4'>
+            <img src={girl} alt='girl' />
+          </Column>
+          <Column size='u-1-2 u-md-1-4'>
+            <img src={girl} alt='girl' />
+          </Column>
+          <Column size='u-1-2 u-md-1-4'>
+            <img src={girl} alt='girl' />
+          </Column>
+          <Column size='u-1-2 u-md-1-4'>
+            <img src={girl} alt='girl' />
+          </Column>
+          <Column size='u-1-2 u-md-1-4'>
+            <img src={girl} alt='girl' />
+          </Column>
+
+        </Grid>
       </Fullpage>
     )
   }
