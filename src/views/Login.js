c22670e8660f88a855c4fe27a70738645b7b378d
Maxx Lehmann <mglehman@mtu.edu>
2017-11-17 21:50:33 +0100

    call login correctly

--- src/views/Login.js
+++ src/views/Login.js
@@ -3,6 +3,8 @@ import '../css/Login.css'
 import Fullpage from '../components/Fullpage'
 import Header from '../components/Header'
 import { consumer } from '../utils/consumer'
+import TextField from '../components/TextField'
+import Button from '../components/Button'
 
 export default class Login extends Component {
   constructor(props) {
@@ -10,7 +12,7 @@ export default class Login extends Component {
     this.state = { email: '', password: ''}
   }
 
-  onGet = (name, value) => this.setState({[name]: value})
+  onChange = (e) => this.setState({[e.target.name]: e.target.value})
 
   consume = () => {
     consumer('', 'POST', this.state)
@@ -22,10 +24,13 @@ export default class Login extends Component {
 
   render() {
     return (
-      <Fullpage>
-        <div>
-          <Header>Login</Header>
-        </div>
+      <Fullpage className='login'>
+        <Header>Login</Header>
+        <TextField type='email' onChange={this.onChange} placeholder='Email'
+          name='email' />
+        <TextField type='password' onChange={this.onChange}
+          placeholder='Password' name='password' />
+        <Button primary click={this.consume}>Login</Button>
       </Fullpage>
     )
   }
