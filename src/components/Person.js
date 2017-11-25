c6a081ee0e826847eb32f4d0d74ec9fb13a12e73
Maxx <maxx.g.lehmann@continental-corporation.com>
2017-11-25 10:43:23 +0100

    subsubroutes to pic

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, Redirect } from 'react-router-dom'
 import circle from '../static/circle.svg'
 import circleGreen from '../static/circle_green.svg'
 
 export default class Home extends Component {
-  prospect = () => this.props.pics.map((prospect, index) => (
-        <img className='home-img' key={index} src={prospect} alt='person' />
-      ))
+  prospect = (props) => {
+      const i = parseInt(props.match.params.pic, 10)
+      if (!(i >= 0 && i < 5)) return <Redirect to='/home' />
+      return <img className='home-img' src={this.props.pics[i]} alt='person' />
+    }
 
   render() {
     return (
