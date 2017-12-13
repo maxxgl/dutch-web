e19453f14ac92a0c4589511246043383c3752ecb
Maxx <maxx.g.lehmann@continental-corporation.com>
2017-12-13 11:23:51 +0100

    added consumer

--- src/views/Dates.js
+++ src/views/Dates.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+// import { Switch, Route, Link, Redirect } from 'react-router-dom'
+import '../css/Dates.css'
 import Fullpage from '../components/Fullpage'
 import Head from '../components/Head'
+import { consumer } from '../utils/consumer'
 
 export default class Dates extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { dates: [] }
+    this.consume()
+  }
+
+  consume = () => {
+    consumer('user/' + localStorage.getItem('userId') + '/date/', 'GET', "")
+      .then((response) => {
+        if (response) {
+          this.setState({ dates: response })
+        }})
+  }
+
   render() {
     return (
       <Fullpage>
         <Head />
+        {this.state.dates}
       </Fullpage>
     )
   }
