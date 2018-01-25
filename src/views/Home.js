f36813d03768841fb4ea977ab0b53d6e0d0f0ea8
Maxx Lehmann <mglehman@mtu.edu>
2018-01-25 19:27:27 +0100

    updated shuffle to PUT when 2 matches selected

--- src/views/Home.js
+++ src/views/Home.js
@@ -13,9 +13,10 @@ export default class Home extends Component {
     this.state = { prospects: [], remove: [0, 0, 0, 0, 0] }
     this.consume()
   }
+  userId = localStorage.getItem('userId')
 
   consume = () => {
-    consumer('user/' + localStorage.getItem('userId') + '/match/', 'GET', "")
+    consumer('user/' + this.userId + '/match/', 'GET', "")
       .then((response) => {
         if (response) {
           this.setState({ prospects: response })
@@ -24,11 +25,14 @@ export default class Home extends Component {
 
   shuffle = () => {
     let ids = []
+    let method = 'PUT'
     for (var i = 0; i < 5; i++) {
-      if (this.state.remove[i]) ids.push(this.state.prospects[i]._id)
+      if (this.state.remove[i]) ids.push(this.state.prospects[i].match.$oid)
     }
-    consumer('user/' + localStorage.getItem('userId') + '/match/',
-      'DELETE', { ids: ids })
+    if (ids.length === 2) {
+      method = 'POST'
+    }
+    consumer('user/' + this.userId + '/match/', method, { ids: ids })
       .then((response) => {
         if (response) {
           this.setState({ remove: [0, 0, 0, 0, 0] })
